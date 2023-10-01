9d3aef96fc8454fd65ad58be80653262f339acb1
NosborCT <66808917+NosborCT@users.noreply.github.com>
2023-10-01 11:59:52 -0300

    feat: Criação do projeto

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+const botoes = document.querySelectorAll('.botao');
+const casas = document.querySelectorAll('.casa');
+
+
+
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarCasa();
+
+        botao.classList.add("selecionado");
+        casas[indice].classList.add("selecionado")
+
+
+    })
+})
+
+
+function desselecionarCasa() {
+    const casaSelecionada = document.querySelector(".casa.selecionado");
+    casaSelecionada.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado")
+}
